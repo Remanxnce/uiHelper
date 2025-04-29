c7eeda6d515ce998f57e8e8455f41ff1e0d2d550
Remanxnce <155000723+Remanxnce@users.noreply.github.com>
2025-04-29 09:55:17 -0400

    created src file

---
+++ src/uiHelper.js
@@ -0,0 +1,54 @@
+export class uiHelper {
+    /**
+     * 
+     * @param {*} player 
+     * @param {import("./uiHelper").smallPanelContent} content 
+     */
+    static sendOverheadNotification(player, content){
+        //ui.notification_2.
+    }
+    /**
+     * 
+     * @param {*} player 
+     * @param {import("./uiHelper").smallPanelContent} content 
+     */
+    static sendSidescreenNotification(player, content){
+        //ui.notification_1.
+    }
+    /**
+     * 
+     * @param {*} player 
+     * @param {string} scoreboard 
+     */
+    static setScoreboard(player, scoreboard){
+        //ui.scoreboard.
+    }
+    /**
+     * 
+     * @param {*} player 
+     * @param {import("./uiHelper").smallPanelContent} content 
+     */
+    static setPromoPanel(player, content){
+        //ui.promo.
+    }
+    /**
+     * 
+     * @param {*} player 
+     * @param {string} content 
+     */
+    static setEffectPanel(player, content){
+        //ui.effects.
+    }
+    
+    static setAbsorptionPanel(player, content){
+        //ui.abs.
+    }
+    /**
+     * 
+     * @param {*} player 
+     * @param {number} duration 
+     */
+    static setTimerBarDuration(player, duration){
+
+    }
+}
---
+++ src/uiHelper.ts
@@ -0,0 +1,5 @@
+export interface smallPanelContent {
+    image: string;
+    title:string;
+    description:string;
+}
